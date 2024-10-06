025f31b42c0464a05b237c2ab92de2d00fb945d0
Yadzim <orinxojayevazizxon@gmail.com>
2024-10-06 14:15:51 +0500

    fghfgh

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="# 000000" />
+        <link rel="icon" href="./favicon.ico" />
+
+        <title>EMA Logistics</title>
+        <meta name="title" content="EMA Logistics" />
+        <meta name="description" content="EMA Logistika servisida 7 yillik tajriba Xitoydan O'zbekistonga: Avto, Avia, Temiryo'l kargosi. Xitoyning naqd 5 ta shahrida omborlarimiz mavjud: Yivu Urumchi Guangzhou Shengjin Va eng asosiy qulaylik esa bojxona ishlari va door to door" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://emalogistics.uz" />
+        <meta property="og:title" content="EMA Logistics" />
+        <meta property="og:description" content="EMA Logistika servisida 7 yillik tajriba Xitoydan O'zbekistonga: Avto, Avia, Temiryo'l kargosi. Xitoyning naqd 5 ta shahrida omborlarimiz mavjud: Yivu Urumchi Guangzhou Shengjin Va eng asosiy qulaylik esa bojxona ishlari va door to door" />
+        <meta property="og:image" content="/favicon.ico" />
+
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Noto+Serif:ital,wght@0,100..900;1,100..900&family=SUSE:wght@100..800&display=swap" rel="stylesheet" />
