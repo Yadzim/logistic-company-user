228cd715827d47e82d2dda677f4f76e9e9b75159
Yadzim <orinxojayevazizxon@gmail.com>
2024-10-06 14:09:36 +0500

    .....

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,16 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "EMA logistics",
-  description: "Logistika servisida 7 yillik tajriba Xitoydan O'zbekistonga: Avto, Avia, Temiryo'l kargosi. Xitoyning naqd 5 ta shahrida omborlarimiz mavjud: Yivu Urumchi Guangzhou Shengjin Va eng asosiy qulaylik esa bojxona ishlari va door to door",
+  title: "EMA Logistics",
+  description: "EMA Logistika servisida 7 yillik tajriba Xitoydan O'zbekistonga: Avto, Avia, Temiryo'l kargosi. Xitoyning naqd 5 ta shahrida omborlarimiz mavjud: Yivu Urumchi Guangzhou Shengjin Va eng asosiy qulaylik esa bojxona ishlari va door to door",
+  openGraph: {
+    title: 'EMA Logistika',
+    description: "EMA Logistika servisida 7 yillik tajriba Xitoydan O'zbekistonga: Avto, Avia, Temiryo'l kargosi. Xitoyning naqd 5 ta shahrida omborlarimiz mavjud: Yivu Urumchi Guangzhou Shengjin Va eng asosiy qulaylik esa bojxona ishlari va door to door",
+    type: 'website',
+    locale: 'uz_UZ',
+    url: 'https://emalogistics.uz',
+    siteName: 'EMA Logistics',
+  },
 };
 
 export default function RootLayout({
---
+++ next-seo.config.ts
@@ -0,0 +1,14 @@
+export default {
+  title: "EMA Logistics",
+  description:
+    "EMA Logistika servisida 7 yillik tajriba Xitoydan O'zbekistonga: Avto, Avia, Temiryo'l kargosi. Xitoyning naqd 5 ta shahrida omborlarimiz mavjud: Yivu Urumchi Guangzhou Shengjin Va eng asosiy qulaylik esa bojxona ishlari va door to door",
+  openGraph: {
+    title: "EMA Logistika",
+    description:
+      "EMA Logistika servisida 7 yillik tajriba Xitoydan O'zbekistonga: Avto, Avia, Temiryo'l kargosi. Xitoyning naqd 5 ta shahrida omborlarimiz mavjud: Yivu Urumchi Guangzhou Shengjin Va eng asosiy qulaylik esa bojxona ishlari va door to door",
+    type: "website",
+    locale: "uz_UZ",
+    url: "https://emalogistics.uz",
+    siteName: "EMA Logistics",
+  },
+};
